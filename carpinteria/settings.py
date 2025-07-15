b14e1bd7234bc3af41d3f604c85bd13eb2d02ca1
hector24 <hectortigre119@gmail.com>
2025-07-15 16:23:27 -0500

    actualizacion del settings

--- carpinteria/settings.py
+++ carpinteria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-16ldgwz0qxvyr%gjgm8f7kvyg@ihn32gam#d=tws@z1h^*8)+v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carpinteria-jhr7.onrender.com']
 
 
 # Application definition
